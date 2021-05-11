528d79c16c62e7791c36a668f31d8ba46ac8e61c
Sven Blanke <sven.blanke@software-architects.de>
2021-05-11 14:57:28 +0200

    astrid layout for blog entries

--- src/components/article.js
+++ src/components/article.js
@@ -1,59 +1,50 @@
 import React from "react";
 import { Link } from "gatsby";
-import Img from "gatsby-image";
 
-const Card = ({ article }) => {
+const Article = ({ article }) => {
   return (
-    <Link to={`/article/${article.node}`} className="uk-link-reset">
-      <div className="posts-layout">
-        <article className="post type-post">
-          <div className="uk-card-media-top">
-            {article.node.image && article.node.image.childImageSharp && (
-              <Img
-                fixed={article.node.image.childImageSharp.fixed}
-                imgStyle={{ position: "static" }}
-              />
-            )}
-          </div>
-          <div className="uk-card-body">
-            <header className="entry-header entry-header-blog">
-              <h2 className="entry-title" style={{color: "black"}}>
-                {article.node.title}
-              </h2>
-              <div className="subtitle">
-                <div className="entry-meta noBorder">
-                  <span className="posted-on"><i className="fa fa-calendar"></i><time>{article.node.published_at}</time></span>
-                  {article.node.author && (<span className="byline"><i className="fa fa-user"></i>{article.node.author.name}</span>)}
-                  <span className="cat-links">0 comments</span>
-                </div>
-              </div>
-            </header>
-            <div className="entry-content entry-content-blog">
-              <p>
-                {article.node.description}
-              </p>
-              {article.node.content.map((content) => {
-                return ( <div style={{width: "400px"}}><p style={{"white-space":"nowrap", "overflow": "hidden", "text-overflow": "ellipsis"}}>{content.content}</p></div> )
-              })}
-              <div>
-                <hr className="uk-divider-small" />
-                <div className="uk-grid-small uk-flex-left" data-uk-grid="true">
-                  <div>
-                    {article.node.author && article.node.author.picture && (
-                      <Img
-                        fixed={article.node.author.picture.childImageSharp.fixed}
-                        imgStyle={{ position: "static", borderRadius: "50%" }}
-                      />
-                    )}
-                  </div>
-              </div>
-            </div>
+    <article id="post-469" className="noBorder post-469 post type-post status-publish format-standard has-post-thumbnail hentry category-blogs category-designers category-development clearfix">
+      <header className="entry-header entry-header-blog">
+        <h2 className="entry-title">
+          <Link to={`/article/${article.strapiId}`} rel="Bookmark">
+            {article.title}
+          </Link>
+        </h2>					
+        <div className="subtitle">
+          <div className="entry-meta noBorder">
+            <span className="posted-on"><i className="fa fa-calendar"></i>
+              <a href="https://www.kuesa.com/kuesa-3d-studio-1-3-is-released/" rel="bookmark">
+                <time className="entry-date published" dateTime={article.published_at}>{article.published_at}</time>
+                <time className="updated" dateTime="2021-05-04T18:18:12+00:00">May 4, 2021</time>
+              </a>
+            </span>
+            <span className="byline"> <i className="fa fa-user"></i>
+              <span className="author vcard">
+                <a className="url fn n" href="https://www.kuesa.com/author/timobuske/">{article.author.name}</a>
+              </span>
+            </span>
+            <span className="cat-links">0 comments</span>						
             </div>
           </div>
-        </article>
-      </div>
-    </Link>
+      </header>
+        <div className="entry-content entry-content-blog">
+                <div className="entry-thumb">
+            { article.image?.publicURL &&       
+              <Link to={`/article/${article.strapiId}`}>
+                <img style={{maxWidth:"520px", maxHeight:"187px"}} src={article.image.publicURL} 
+                  className="attachment-astrid-medium-thumb size-astrid-medium-thumb wp-post-image" alt="" loading="lazy" />
+              </Link>
+            }              
+          </div>
+              <div className="entry-summary">
+          <p style={{display:"-webkit-box", WebkitLineClamp:"4", WebkitBoxOrient:"vertical", overflow:"hidden"}}>{article.content[0].content}</p>
+        </div>
+        <div className="read-more clearfix">
+          <Link to={`/article/${article.strapiId}`} className="button post-button readMoreButton">Read more</Link>
+        </div>
+      </div> 
+    </article>
   );
 };
 
-export default Card;
+export default Article;
